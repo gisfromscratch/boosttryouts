1363feb9a9840d72cf9d73f813cf10a416424bbd
J.T <gisfromscratch@live.de>
2015-03-04 00:55:42 +0100

    First two samples after building boost with bootstrap b2

---
+++ BoostShell/BoostShell/stdafx.h
@@ -0,0 +1,21 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+// Boost
+#include <boost/lambda/lambda.hpp>
+#include <boost/regex.hpp>
+
+
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <string>
